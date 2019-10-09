431eddd933b80ac23d2d39afc6cd6684e6b9f678
prasantib <prasanti.babu@gmail.com>
2019-10-09 17:44:54 -0400

    StringBoot_402

---
+++ src/main/java/com/example/demo/StringBoot402Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot402Application.class, args);
+    }
+
+}
